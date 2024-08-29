d4204bececa69fa4793fd2c08fe58961afcd4028
Morshed Siam <morshedsiam33@gmail.com>
2024-08-29 10:34:23 +0600

    Server Is Ready

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://car-doctor-server-eosin-sigma.vercel.app/"
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vkv4ktv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
